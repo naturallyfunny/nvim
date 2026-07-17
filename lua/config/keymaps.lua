d3d1e0c925572d52f76417b4e4cd22dc3d8c342f
Ardian <ardian@naturallyfunny.dev>
2026-07-17 09:52:23 +0700

    feat(keymaps): add <leader>yl to copy line with file path and optional visual selection

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -62,3 +62,55 @@ end
 
 vim.keymap.set("n", "<leader>y]d", function() copy_diag_nav("next") end, { desc = "Yank next diagnostic to clipboard" })
 vim.keymap.set("n", "<leader>y[d", function() copy_diag_nav("prev") end, { desc = "Yank prev diagnostic to clipboard" })
+
+-- Copy current line along with file path and line number
+-- If in visual mode, also appends the visual selection in a clean blockquote & bullet format
+local function copy_line_with_path(is_visual)
+  -- Gunakan ":~" agar path disingkat menggunakan "~/" (misal ~/dev/...)
+  local file = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~")
+  local lnum = vim.api.nvim_win_get_cursor(0)[1] -- Nomor baris aktif
+  local line_content = vim.api.nvim_get_current_line() -- Isi baris aktif
+
+  local text = string.format("%s:%d\n> %s", file, lnum, line_content)
+
+  if is_visual then
+    -- Ambil koordinat seleksi visual
+    local pos1 = vim.fn.getpos("v")
+    local pos2 = vim.fn.getpos(".")
+    local r1, c1 = pos1[2], pos1[3]
+    local r2, c2 = pos2[2], pos2[3]
+
+    -- Urutkan koordinat agar r1, c1 berada sebelum r2, c2
+    if r1 > r2 or (r1 == r2 and c1 > c2) then
+      r1, c1, r2, c2 = r2, c2, r1, c1
+    end
+
+    local region
+    if vim.fn.getregion then
+      -- Neovim 0.10+
+      local mode = vim.fn.mode()
+      region = vim.fn.getregion({0, r1, c1, 0}, {0, r2, c2, 0}, { type = mode })
+    else
+      -- Fallback untuk Neovim versi lama
+      region = vim.api.nvim_buf_get_text(0, r1 - 1, c1 - 1, r2 - 1, c2, {})
+    end
+
+    local selected_text = table.concat(region, "\n")
+    local has_newline = string.find(selected_text, "\n") ~= nil
+    if has_newline then
+      text = string.format("%s\n- Selected:\n%s", text, selected_text)
+    else
+      text = string.format("%s\n- Selected: `%s`", text, selected_text)
+    end
+    
+    -- Keluar dari visual mode secara bersih
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+  end
+
+  vim.fn.setreg("+", text)
+  vim.notify("Copied: " .. text, vim.log.levels.INFO)
+end
+
+vim.keymap.set("n", "<leader>yl", function() copy_line_with_path(false) end, { desc = "Yank line with file path and line number" })
+vim.keymap.set("x", "<leader>yl", function() copy_line_with_path(true) end, { desc = "Yank line with file path and selection" })
+
